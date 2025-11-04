20c691193a4549d1c04962abc794622f5a00552a
Akshata307 <1nc23cs010@ncetmail.com>
2025-11-04 15:13:32 +0530

    Update and rename Loss Percentage.cc to Area and Perimeter of Square.c

---
+++ Area and Perimeter of Square.c
@@ -0,0 +1,10 @@
+// Given side of the square.Calculate the Area and perimeter of a square. 
+#include<stdio.h>
+int main()
+{
+    int side;
+scanf("%d
+",&side);
+    printf("%d %d",side*side,4*side);
+return 0;
+}
