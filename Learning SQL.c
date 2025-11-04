9153a87d8628b52c7d95f1b6e75b0b5b7b351dfa
Akshata307 <1nc23cs010@ncetmail.com>
2025-11-04 21:42:53 +0530

    Create Learning SQL.c

---
+++ Learning SQL.c
@@ -0,0 +1,11 @@
+Kumar has recently started learning from the new SQL course.
+He has a table which initially has R rows and C columns. He then adds E extra rows to it. How many total cells does he have finally?#include <stdio.h>
+
+int main()
+{
+    int R, C, E;
+    scanf("%d %d %d", &R, &C, &E);
+    int total = (R + E) * C;
+    printf("%d", total);
+    return 0;
+}
