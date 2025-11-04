e07c66eea579ba3e76060d61b1757d35ed37f5db
Akshata307 <1nc23cs010@ncetmail.com>
2025-11-04 15:33:45 +0530

    Create Average of Two Numbers.c

---
+++ Average of Two Numbers.c
@@ -0,0 +1,9 @@
+// You'll be given 2 integers num1 and num2, find the Average of those 2 integers.
+#include<stdio.h>
+int main()
+{
+int a,b;
+scanf("%d %d",&a,&b);
+printf("Avarage of %d and %d is:%.2f",a,b,(a+b)/2.0);
+return 0;
+}
