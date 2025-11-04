6b4449b15d8b4d7c368efcab7a75352ac12373d4
Akshata307 <1nc23cs010@ncetmail.com>
2025-11-04 15:22:30 +0530

    Create Required Points.c

---
+++ Required Points.c
@@ -0,0 +1,10 @@
+//Pavan Playing a mobile game. In the game Pavan's character is "Unstoppable force" that means he can perform special attack.However for each special attack it costs A points to "Unstoppable force". If Pavan currently has B points,determine the number of special attacks he performed.
+
+#include<stdio.h>
+int main()
+{
+int A,B;
+scanf("%d",&A,&B);
+printf("%d",B/A);
+return 0;
+}
